20cc586e338b84154cb51aaff2b9d048d915ff3c
Michal <michalpalkowski3@wp.pl>
2024-12-05 16:04:45 +0100

    update

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,25 +1,59 @@
 pub mod transform;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+use itertools::Itertools;
+use starknet_crypto::Felt;
 use std::path::PathBuf;
-pub use swiftness_proof_parser::*;
-pub use swiftness_stark::*;
-pub use transform::TransformTo;
+pub use swiftness_proof_parser::parse;
+pub use swiftness_stark::config::StarkConfig;
 
-#[cfg(feature = "dex")]
-use swiftness_air::layout::dex::Layout;
-#[cfg(feature = "dynamic")]
-use swiftness_air::layout::dynamic::Layout;
-#[cfg(feature = "recursive")]
-use swiftness_air::layout::recursive::Layout;
-#[cfg(feature = "recursive_with_poseidon")]
-use swiftness_air::layout::recursive_with_poseidon::Layout;
-#[cfg(feature = "small")]
-use swiftness_air::layout::small::Layout;
-#[cfg(feature = "starknet")]
-use swiftness_air::layout::starknet::Layout;
-#[cfg(feature = "starknet_with_keccak")]
-use swiftness_air::layout::starknet_with_keccak::Layout;
+use swiftness_air::layout::dex::Layout as LayoutDex;
+use swiftness_air::layout::recursive::Layout as LayoutRecursive;
+use swiftness_air::layout::recursive_with_poseidon::Layout as LayoutRecursiveWithPoseidon;
+use swiftness_air::layout::small::Layout as LayoutSmall;
+use swiftness_air::layout::starknet::Layout as LayoutStarknet;
+use swiftness_air::layout::starknet_with_keccak::Layout as LayoutStarknetWithKeccak;
+
+use swiftness_air::public_memory::STONE_6_ENABLED;
+use swiftness_commitment::table::decommit::{HASHER_248_LSB, HASHER_BLAKE2S};
+use swiftness_fri::fri::{CONST_STATE, VAR_STATE, WITNESS};
+use swiftness_stark::stark::Error;
+use swiftness_stark::types::StarkProof;
+
+mod serialize;
+use crate::serialize::serialize;
+use std::format;
+use std::fs::write;
+use swiftness::transform_stark::TransformTo;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[clap(rename_all = "snake_case")]
+enum Layout {
+    Dex,
+    Recursive,
+    RecursiveWithPoseidon,
+    Small,
+    Starknet,
+    StarknetWithKeccak,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum StoneVersion {
+    Stone5,
+    Stone6,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Hasher {
+    #[clap(name = "keccak_160_lsb")]
+    Keccak160Lsb,
+    #[clap(name = "keccak_248_lsb")]
+    Keccak248Lsb,
+    #[clap(name = "blake2s_160_lsb")]
+    Blake2s160Lsb,
+    #[clap(name = "blake2s_248_lsb")]
+    Blake2s248Lsb,
+}
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -27,13 +61,68 @@ struct CairoVMVerifier {
     /// Path to proof JSON file
     #[clap(short, long)]
     proof: PathBuf,
+
+    /// Output directory for the generated files
+    #[clap(short, long, default_value = "calldata")]
+    out: PathBuf,
+
+    /// Layout
+    #[clap(short, long)]
+    layout: Layout,
+
+    /// Hasher
+    #[clap(long)]
+    hasher: Hasher,
+
+    /// Stone version
+    #[clap(short, long)]
+    stone_version: StoneVersion,
+}
+
+fn verify_layout(
+    layout: Layout,
+    stark_proof: StarkProof,
+    security_bits: Felt,
+) -> Result<(), Error> {
+    match layout {
+        Layout::Dex => stark_proof.verify::<LayoutDex>(security_bits),
+        Layout::Recursive => stark_proof.verify::<LayoutRecursive>(security_bits),
+        Layout::RecursiveWithPoseidon => {
+            stark_proof.verify::<LayoutRecursiveWithPoseidon>(security_bits)
+        }
+        Layout::Small => stark_proof.verify::<LayoutSmall>(security_bits),
+        Layout::Starknet => stark_proof.verify::<LayoutStarknet>(security_bits),
+        Layout::StarknetWithKeccak => stark_proof.verify::<LayoutStarknetWithKeccak>(security_bits),
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = CairoVMVerifier::parse();
-    let stark_proof = parse(std::fs::read_to_string(cli.proof)?)?.transform_to();
+    let input = std::fs::read_to_string(cli.proof)?;
+    let stark_proof = parse(input.clone())?.transform_to();
     let security_bits = stark_proof.config.security_bits();
-    let result = stark_proof.verify::<Layout>(security_bits)?;
-    println!("{:?}", result);
+    unsafe {
+        STONE_6_ENABLED = cli.stone_version == StoneVersion::Stone6;
+        HASHER_BLAKE2S = cli.hasher == Hasher::Blake2s160Lsb || cli.hasher == Hasher::Blake2s248Lsb;
+        HASHER_248_LSB = cli.hasher == Hasher::Keccak248Lsb || cli.hasher == Hasher::Blake2s248Lsb;
+    };
+    verify_layout(cli.layout, stark_proof, security_bits)?;
+
+    let (const_state, mut var_state, mut witness) =
+        unsafe { (CONST_STATE.clone(), VAR_STATE.clone(), WITNESS.clone()) };
+    let initial = serialize(input)?
+        .split_whitespace()
+        .map(|s| Felt::from_dec_str(s).unwrap().to_hex_string())
+        .join(" ");
+
+    let final_ = format!("{} {} {}", const_state, var_state.pop().unwrap(), witness.pop().unwrap());
+
+    write(cli.out.join("initial"), initial)?;
+    write(cli.out.join("final"), final_)?;
+
+    for (i, (v, w)) in var_state.iter().zip(witness.iter()).enumerate() {
+        write(cli.out.join(format!("step{}", i + 1)), format!("{} {} {}", const_state, v, w))?;
+    }
+    print!("{}", var_state.len());
     Ok(())
 }
---
+++ cli/verify.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 <job_id> <layout> <hasher> <stone_version> <memory_verification>"
+    exit 1
+fi
+
+string_to_hex() {
+    input_string="$1"
+    hex_string="0x"
+    for ((i = 0; i < ${#input_string}; i++)); do
+        hex_char=$(printf "%x" "'${input_string:$i:1}")
+        hex_string+=$hex_char
+    done
+    echo "$hex_string"
+}
+
+generate_random_job_id() {
+    echo "0x$(openssl rand -hex 8)"
+}
+
+job_id=$1
+if [[ "$job_id" == "random" ]]; then
+    job_id=$(generate_random_job_id)
+fi
+
+# job_id=$1
+layout=$(string_to_hex $2)
+hasher=$(string_to_hex $3)
+stone_version=$(string_to_hex $4)
+memory_verification=$(string_to_hex $5)
+
+send_transaction() {
+    local retries=5
+    local count=0
+    local status=0
+
+    while [[ $count -lt $retries ]]; do
+        sncast \
+            --wait \
+            invoke \
+            --contract-address "$(<calldata/contract_address)" \
+            --function "$1" \
+            --calldata "$3 $(<$2)"
+
+        sleep 5 # extra delay to make sure the transaction is registered
+
+        status=$?
+
+        if [[ $status -eq 0 ]]; then
+            return 0
+        else
+            echo "Transaction failed with status $status. Retrying... ($((count + 1))/$retries)"
+        fi
+
+        count=$((count + 1))
+    done
+
+    echo "Transaction failed after $retries attempts."
+    return $status
+}
+
+echo ""
+echo "Sending verify_proof_initial"
+send_transaction "verify_proof_initial" "calldata/initial" "$job_id $layout $hasher $stone_version $memory_verification"
+
+i=1
+while true; do
+    filename="calldata/step${i}"
+
+    if [[ -e "$filename" ]]; then
+        echo ""
+        echo "Sending verify_proof_step (${i})"
+        send_transaction "verify_proof_step" "$filename" "$job_id"
+    else
+        break
+    fi
+
+    ((i++))
+done
+
+echo ""
+echo "Sending verify_proof_final_and_register_fact"
+send_transaction "verify_proof_final_and_register_fact" "calldata/final" "$job_id"
